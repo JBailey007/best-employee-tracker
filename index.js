260b9ce92af43a75d83df0eebb1a6267eca3d574
Jalen <jwbailey13@gmail.com>
2023-11-02 21:02:34 -0600

    added .env file, added view all departments and view all roles cases

--- index.js
+++ index.js
@@ -8,12 +8,40 @@ const dbPassword = process.env.DB_PASSWORD;
 const dbName = process.env.DB_NAME;
 
 // Connection to database
-const db = mysql.createConnection(
-  {
-    host: "localhost",
-    user: dbUser,
-    password: dbPassword,
-    database: dbName,
-  },
-  console.log(`Connected to the ${dbName} database.`)
-);
+async function dbConnection(select) {
+  try {
+    const db = await mysql.createConnection({
+      host: "localhost",
+      user: dbUser,
+      password: dbPassword,
+      database: dbName,
+    });
+
+    let receivedRowsFromDb = [];
+    let receivedOutputFromInquirer = [];
+
+    switch (select) {
+      case "View All Departments":
+        receivedRowsFromDb = await db.query("SELECT * FROM department");
+        console.table(receivedRowsFromDb[0]);
+        break;
+
+        case "View All Roles":
+          receivedRowsFromDb = await db.query(`
+            SELECT
+              role.id, 
+              role.title,
+              role.salary,
+              department.name AS department
+            FROM role
+            JOIN department ON role.department_id = department.id`);
+            console.table(receivedRowsFromDb[0]);
+            break;
+
+
+    }
+  } catch (err) {
+    console.log(err);
+  }
+}
+
